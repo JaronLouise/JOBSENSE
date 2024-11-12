a77c77532320fafa25a57b6cdd52282f68f23d40
Rose Ann Aguilar <123806801+roseann11@users.noreply.github.com>
2024-11-12 08:30:12 +0800

    Update logreg.js

--- js/logreg.js
+++ js/logreg.js
@@ -116,3 +116,11 @@ window.onload = function () {
     });
 
 };
+
+// Log in (show password)
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.type === 'password' ? 'text' : 'password';
+    passwordField.type = type;
+});
+
