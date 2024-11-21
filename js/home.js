fd7af0a080ab172e89ad64ffb093becfe763f408
JaronLouise <123366766+JaronLouise@users.noreply.github.com>
2024-11-21 17:09:09 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,20 +1,3 @@
-//Load navbar.html content
-document.addEventListener('DOMContentLoaded', () => {
-    const navbarContainer = document.getElementById('navbar-container');
-    fetch('navbar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            navbarContainer.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading navbar:', error);
-        });
-});
 
 // Import the necessary Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
