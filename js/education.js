e5dea58d9ee2fe435e98291f9bf9fb0c89d862aa
JaronLouise <123366766+JaronLouise@users.noreply.github.com>
2024-11-21 17:08:39 +0800

    Update education.js

--- js/education.js
+++ js/education.js
@@ -1,20 +1,3 @@
-// Load navbar.html content
-document.addEventListener('DOMContentLoaded', () => {
-    const navbarContainer = document.getElementById('navbar-container');
-    fetch('navbar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            navbarContainer.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error loading navbar:', error);
-        });
-});
 
 document.addEventListener('DOMContentLoaded', () => {
     const content = document.querySelector('.content');
